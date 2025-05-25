976e08a65a89e630ab6bc6216d935a84369ab2a1
Rckov <Rychkov.dev@yandex.ru>
2025-05-25 04:10:44 +0300

    Update summart for extensions

--- src/RemoteDesktop/Extensions/Extensions.cs
+++ src/RemoteDesktop/Extensions/Extensions.cs
@@ -11,11 +11,9 @@ namespace RemoteDesktop.Extensions;
 internal static class Extensions
 {
     /// <summary>
-    /// Converts a collection of <see cref="Server"/> objects into a collection of <see cref="TreeItemViewModel"/> objects.
+    /// Converts a collection of <see cref="Server"/> into <see cref="TreeItemViewModel"/>.
     /// </summary>
-    /// <param name="items">The collection of <see cref="Server"/> objects to convert. Cannot be <see langword="null"/>.</param>
-    /// <returns>An <see cref="IEnumerable{T}"/> containing <see cref="TreeItemViewModel"/> objects corresponding to the input
-    /// <see cref="Server"/> objects.</returns>
+    /// <param name="items">Collection of servers, must not be null.</param>
     public static IEnumerable<TreeItemViewModel> ToTreeItems(this IEnumerable<Server> items)
     {
         CheckIsNull(items);
@@ -23,11 +21,9 @@ internal static class Extensions
     }
 
     /// <summary>
-    /// Converts a collection of <see cref="ServerGroup"/> objects into a collection of <see cref="TreeItemViewModel"/> objects.
+    /// Converts a collection of <see cref="ServerGroup"/> into <see cref="TreeItemViewModel"/>.
     /// </summary>
-    /// <param name="items">The collection of <see cref="ServerGroup"/> objects to convert. Cannot be <see langword="null"/>.</param>
-    /// <returns>An <see cref="IEnumerable{T}"/> containing <see cref="TreeItemViewModel"/> objects,  where each item corresponds
-    /// to a <see cref="ServerGroup"/> in the input collection.</returns>
+    /// <param name="items">Collection of server groups, must not be null.</param>
     public static IEnumerable<TreeItemViewModel> ToTreeItems(this IEnumerable<ServerGroup> items)
     {
         CheckIsNull(items);
@@ -35,14 +31,13 @@ internal static class Extensions
     }
 
     /// <summary>
-    /// Validates that the specified value is not null or, in the case of a string, not empty or whitespace.
+    /// Checks if the value is null.
     /// </summary>
-    /// <typeparam name="T">The type of the value to validate.</typeparam>
-    /// <param name="value">The value to check for null or invalid string content.</param>
-    /// <param name="paramName">The name of the parameter being validated. This is automatically populated with the caller's member name if not
-    /// explicitly provided.</param>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> is a string that is empty or consists only of whitespace.</exception>
+    /// <typeparam name="T">Type of the value.</typeparam>
+    /// <param name="value">Value to check.</param>
+    /// <param name="paramName">Parameter name for exception (auto-filled).</param>
+    /// <exception cref="ArgumentNullException">If value is null.</exception>
+    /// <exception cref="ArgumentException">If string is empty or whitespace.</exception>
     public static void CheckIsNull<T>(T value, [CallerMemberName] string paramName = "")
     {
         if (value is null)
